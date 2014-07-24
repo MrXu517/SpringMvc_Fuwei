cd968e39df774d0db7736522c7918cf29a5b2b2e
emilyyufen <emilyyufen@gmail.com>
2014-07-24 10:31:58 +0800

    误删SystemInfoController BUG修复
    Signed-off-by: emilyyufen <emilyyufen@gmail.com>

---
+++ src/com/fuwei/controller/SystemInfoController.java
@@ -0,0 +1,31 @@
+package com.fuwei.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fuwei.commons.SystemCache;
+
+@RequestMapping("/systeminfo")
+@Controller
+public class SystemInfoController extends BaseController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView Index(HttpSession session, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		request.setAttribute("companylist", SystemCache.companylist);
+		request.setAttribute("gongxulist", SystemCache.gongxulist);
+		request.setAttribute("salesmanlist", SystemCache.salesmanlist);
+		request.setAttribute("userlist", SystemCache.userlist);
+		request.setAttribute("rolelist", SystemCache.rolelist);
+		return new ModelAndView("systeminfo/index");
+
+	}
+}
--- src/com/fuwei/controller/SystemInitController.java
+++
@@ -1,6 +0,0 @@
-package com.fuwei.controller;
-
-/*系统初始化功能*/
-public class SystemInitController {
-
-}
